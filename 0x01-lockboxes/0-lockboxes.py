7599b8051de3ebb0033deaa6596b01580347b501
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-01 11:36:20 +0300

    new

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Defines a function that determines if a box containing a list"""
+
+
+
+def canUnlockAll(boxes):
+    """ Determines if boxes can be unlocked"""
+    position = 0
+    unlocked = {}
+
+    for box in boxes:
+        if len(box) == 0 or position == 0:
+            unlocked[postion] = "always_uncloked"
+        for key in box:
+            if key < len(boxes) and key != position:
+                unlocked[key] = key
+        if len(unlocked) == len(boxes):
+            return True
+        postion += 1
+        return False
