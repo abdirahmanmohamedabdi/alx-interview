5a32307eb60b1859e2bcbc4b7559273d6bd65351
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-07 11:46:54 +0300

    new

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+""" Write a method that calculates the fewest number of operations needed
+to result in exactly n H characters in the file. """
+
+
+def minOperations(n: int) -> int:
+    """ calculates the fewest number of operations needed to result
+    in exactly n H characters in the file """
+    process = 2
+    op = 0
+    while n > 1:
+        while n % process == 0:
+            op += process
+            n /= process
+        process += 1
+    return op
