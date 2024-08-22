c514517b8ac622355ca609d43443760752e7ecf7
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-08-22 13:11:53 +0300

    new

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,9 +14,9 @@ def validUTF8(data):
         if byte_count == 0:
             if i >> 5 == 0b110 or i >> 5 == 0b1110:
                 byte_count = 1
-            elif i >> 4 == 0b11110:
+            elif i >> 4 == 0b1110:
                 byte_count = 2
-            elif i >> 3 == 0b111110:
+            elif i >> 3 == 0b11110:
                 byte_count = 3
             elif i >> 7 == 0b1:
                 return False
