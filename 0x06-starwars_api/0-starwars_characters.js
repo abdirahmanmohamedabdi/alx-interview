0cd819fe13cea508bc14abe77c7871b4308a0b69
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-06 01:01:49 +0300

    new

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,10 +3,10 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-async function starwarsCharacters (filmID) {
-  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmID;
+async function starwarsCharacters (filmId) {
+  const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
   let response = await (await request(endpoint)).body;
-  response = JSON.parse(response.body);
+  response = JSON.parse(response);
   const characters = response.characters;
 
   for (let i = 0; i < characters.length; i++) {
