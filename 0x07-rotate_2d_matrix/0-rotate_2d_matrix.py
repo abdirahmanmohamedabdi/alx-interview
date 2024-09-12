eddd8ebee585a4afdddaeda9dceb8b39320d9051
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-12 17:43:44 +0300

    new

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Rotate 2D Matrix
+""" Rotate 2D Matrix.
 """
 
 
 def rotate_2d_matrix(matrix):
-    """ Rotates an m by n  2D Matrix
+    """ Rotates an m by n  2D Matrix.
     """
     if type(matrix) != list:
         return
