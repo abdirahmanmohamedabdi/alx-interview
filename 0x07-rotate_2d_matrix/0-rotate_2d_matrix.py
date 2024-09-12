d72491771f28727885e56d6023a6a7c06371f5ae
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-12 17:50:59 +0300

    new

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -4,24 +4,15 @@
 
 def rotate_2d_matrix(matrix):
     """Rotates an m by n  2D Matrix"""
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = temp
+
+    for i in range(n):
+        for j in range(int(n / 2)):
+            temp = matrix[i][j]
+            matrix[i][j] = matrix[i][n-1-j]
+            matrix[i][n-1-j] = temp
