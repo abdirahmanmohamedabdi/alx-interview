a0da2e60fa28bdad77a32a35b08eb397d4e76aa7
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-09-20 14:25:33 +0300

    new

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Making change
+"""
+
+
+def makeChange(coins, total):
+    """
+    Return the minimum number of coins to make up the total
+    Args:
+        coins: list of the values of the coins in your possession
+        total: total to be made
+    Return:
+        Number of coins is not possible
+    """
+    if total <= 0:
+        return 0
+    if coins == [] or coins is None:
+        return -1
+    try:
+        n = coins.index(total)
+        return 1
+    except ValueError:
+        pass
+
+    coins.sort(reverse=True)
+    coin_count = 0
+    for i in coins:
+        if total % i == 0:
+            coin_count += int(total / i)
+            return coin_count
+        if total - i >= 0:
+            if int(total / i) > 1:
+                coin_count += int(total / i)
+                total = total % i
+            else:
+                coin_count += 1
+                total -= i
+                if total == 0:
+                    break
+    if total > 0:
+        return -1
+    return coin_count
