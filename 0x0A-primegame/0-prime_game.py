7c914a3cf5c9d3c118663099a882e2fa1f546118
Abdirahman Abdi <abdirahmanabdi.ma@gmail.com>
2024-10-02 14:08:29 +0300

    new

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -12,7 +12,7 @@ def primes(n):
     sieve = [True] * (n + 1)
     for p in range(2, n + 1):
         if (sieve[p]):
-            primes.append(p)
+            prime.append(p)
             for i in range(p, n + 1, p):
                 sieve[i] = False
     return prime
